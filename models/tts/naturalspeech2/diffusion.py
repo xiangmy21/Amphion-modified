fix: causal mask for deep layer

models/tts/naturalspeech2/diffusion.py
@@ -106,7 +106,7 @@ class Diffusion(nn.Module):
         h = 1.0 / max(n_timesteps, 1)
         xt = z
         
-        views = get_views(z.shape[-1], window_size=128, stride=32)
+        views = get_views(z.shape[-1], window_size=self.cfg.window_size, stride=self.cfg.stride) # window_size=128, stride=64
         count = torch.zeros_like(xt)
         value = torch.zeros_like(xt)
         

models/tts/naturalspeech2/ns2_inference.py
@@ -101,7 +101,6 @@ class NS2Inference:
         x0, prior_out = self.model.inference(
             ref_code, phone_id, ref_mask, self.args.inference_step, flow=True
         )
-        print("Frames: ", x0.shape[-1])
         # print(prior_out["dur_pred"]) 
         # print(prior_out["dur_pred_round"]) # (1, N) 每个音素的持续帧数
         # print(torch.sum(prior_out["dur_pred_round"])) # 总帧数
@@ -117,6 +116,14 @@ class NS2Inference:
         #     rec_chunk = self.codec.decoder(chunk)
         #     rec_wav_chunks = torch.cat([rec_wav_chunks, rec_chunk], dim=-1)
 
+        print("look_ahead: ", self.cfg.model.prior_encoder.look_ahead)
+        print("dur_blk_size: ", self.cfg.model.prior_encoder.dur_blk_size)
+        print("dur_blk_padding: ", self.cfg.model.prior_encoder.dur_blk_padding)
+        print("pit_blk_size: ", self.cfg.model.prior_encoder.pit_blk_size)
+        print("pit_blk_padding: ", self.cfg.model.prior_encoder.pit_blk_padding)
+        print("multidiffuion window_size: ", self.cfg.model.diffusion.window_size)
+        print("multidiffuion stride: ", self.cfg.model.diffusion.stride)
+        print("Write to file {}".format("out_"+self.args.ref_audio.split('/')[-1].split('.')[0]))
         os.makedirs(self.args.output_dir, exist_ok=True)
 
         sf.write(

models/tts/naturalspeech2/prior_encoder.py
@@ -80,10 +80,10 @@ class PriorEncoder(nn.Module):
         
         # 构造一个phone_id的attention mask, 可以往后看lookahead个音素
         if flow:
-            lookahead = 2
-            phone_attn_mask = torch.zeros(phone_id.shape[1], phone_id.shape[1], device=phone_id.device)
+            look_ahead = self.cfg.look_ahead
+            phone_attn_mask = torch.zeros(phone_id.shape[1], phone_id.shape[1], device=phone_id.device, dtype=torch.bool)
             for i in range(phone_id.shape[1]):
-                phone_attn_mask[i, max(0, i-lookahead):min(i+lookahead+1, phone_id.shape[1])] = 1
+                phone_attn_mask[i, max(0, i-look_ahead):min(i+look_ahead+1, phone_id.shape[1])] = True
         else:
             phone_attn_mask = None
 
@@ -91,8 +91,8 @@ class PriorEncoder(nn.Module):
 
         # 将x切分成长度为blk_size的块,每个块的两边加上padding
         if flow:
-            blk_size = 5
-            padding = 2
+            blk_size = self.cfg.dur_blk_size
+            padding = self.cfg.dur_blk_padding
             dur_pred_out = {
                 "dur_pred_log": torch.zeros(x.shape[0], x.shape[1], device=x.device),
                 "dur_pred": torch.zeros(x.shape[0], x.shape[1], device=x.device),
@@ -120,8 +120,8 @@ class PriorEncoder(nn.Module):
             x, mel_len = self.length_regulator(x, duration, max_len=pitch.shape[1]) # (B, T, d)
 
         if flow:
-            frame_size = 75
-            padding = 25
+            frame_size = self.cfg.pit_blk_size
+            padding = self.cfg.pit_blk_padding
             pitch_pred_log = torch.zeros(x.shape[0], x.shape[1], device=x.device)
             for i in range(0, x.shape[1], frame_size):
                 view = slice(max(0, i-padding), min(i+frame_size+padding, x.shape[1]))

modules/naturalpseech2/transformers.py
@@ -138,6 +138,7 @@ class TransformerEncoderLayer(nn.Module):
             attn_mask_input = ~(attn_mask.bool())
         else:
             attn_mask_input = None
+
         x, _ = self.self_attn(
             query=x, key=x, value=x, key_padding_mask=key_padding_mask_input, attn_mask=attn_mask_input
         )
@@ -228,10 +229,14 @@ class TransformerEncoder(nn.Module):
         else:
             x = self.position_emb(x)  # (B, T, d)
 
-        x = self.layers[0](x, key_padding_mask, attn_mask, condition) # 只在第一层加入attention mask
-
-        for layer in self.layers[1:]:
-            x = layer(x, key_padding_mask, None, condition)
+        if attn_mask is not None:
+            x = self.layers[0](x, key_padding_mask, attn_mask, condition) # 只在第一层加入往后看的attention mask
+            causal_mask = torch.tril(torch.ones(x.size(1), x.size(1), device=x.device, dtype=torch.bool))
+            for layer in self.layers[1:]:
+                x = layer(x, key_padding_mask, causal_mask, condition)
+        else:
+            for layer in self.layers:
+                x = layer(x, key_padding_mask, None, condition)
 
         if self.use_cln: # use_cln即是否使用加入条件的LayerNorm
             x = self.last_ln(x, condition)